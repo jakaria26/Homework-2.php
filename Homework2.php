eb2c54c451ef9c7d3769f10fd9f4cc28a9203313
jakaria26 <jamahmud2022@gmail.com>
2026-02-13 21:24:10 -0500

    I add .php file

---
+++ Homework2.php
@@ -0,0 +1,14 @@
+<?php
+$week = 2;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Homework 2 update</title>
+</head>
+<body>
+  <h1> Good Afternoon <?php echo $week; ?> Branch</h1>
+  <p> This is my homework2 page </p>
+</body>
+</html>
